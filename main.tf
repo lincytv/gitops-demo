b06da9097b7ea6115ccbbc57f267de4303fb675d
Lincy Varghese <lincytv@gmail.com>
2021-09-25 21:18:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo-ec2" {
-  type = "t2.micro"
+  instance_type = "t2.micro"
   ami = "ami-0a57edb3631e68796"
   tags = {
     Name = "demo-club-house"
